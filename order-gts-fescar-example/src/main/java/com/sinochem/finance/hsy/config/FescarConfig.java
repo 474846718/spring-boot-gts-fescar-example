7045eb5f9ef5b2e28fe9e69e6e1fcbb49e64bcc0
heshouyou <18701685713@163.com>
2019-02-18 10:17:56 +0800

    完善数据源配置

--- order-gts-fescar-example/src/main/java/com/sinochem/finance/hsy/config/FescarConfig.java
+++ order-gts-fescar-example/src/main/java/com/sinochem/finance/hsy/config/FescarConfig.java
@@ -35,6 +35,19 @@ public class FescarConfig {
         druidDataSource.setUsername(dataSourceProperties.getUsername());
         druidDataSource.setPassword(dataSourceProperties.getPassword());
         druidDataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+        druidDataSource.setInitialSize(0);
+        druidDataSource.setMaxActive(180);
+        druidDataSource.setMaxWait(60000);
+        druidDataSource.setMinIdle(0);
+        druidDataSource.setValidationQuery("Select 1 from DUAL");
+        druidDataSource.setTestOnBorrow(false);
+        druidDataSource.setTestOnReturn(false);
+        druidDataSource.setTestWhileIdle(true);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(60000);
+        druidDataSource.setMinEvictableIdleTimeMillis(25200000);
+        druidDataSource.setRemoveAbandoned(true);
+        druidDataSource.setRemoveAbandonedTimeout(1800);
+        druidDataSource.setLogAbandoned(true);
         return druidDataSource;
     }
 
